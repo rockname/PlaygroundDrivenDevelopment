698fd55bd8f854b8164576b0a3b84f46e95e6bed
rockname <8536870+rockname@users.noreply.github.com>
2018-08-25 22:10:47 +0900

    apply language and device size to playground liveView.

--- PlaygroundDrivenDevelopment.playground/Pages/Twitter.xcplaygroundpage/Contents.swift
+++ PlaygroundDrivenDevelopment.playground/Pages/Twitter.xcplaygroundpage/Contents.swift
@@ -2,18 +2,21 @@ import UIKit
 import PlaygroundSupport
 import Presentation
 import Mock
+import Domain
 
 let mockContainer = MockContainer.shared
-
+AppEnvironment.language = .ja
 let vc = R.storyboard.twitter.instantiateInitialViewController()!
-PlaygroundPage.current.liveView = vc
+let (parent, _) = playgroundControllers(device: .pad, orientation: .portrait, child: vc)
 
-let statuses = [
-    mockContainer.mockMyStatus,
-    mockContainer.mockStatus01,
-    mockContainer.mockStatus02,
-    mockContainer.mockStatus03,
-    mockContainer.mockTooLongStatus
+PlaygroundPage.current.liveView = parent
+
+let statuses: [Status] = [
+//    mockContainer.mockMyStatus,
+//    mockContainer.mockStatus01,
+//    mockContainer.mockStatus02,
+//    mockContainer.mockStatus03,
+//    mockContainer.mockTooLongStatus
 ]
 
 vc.load(statuses)
